46ba4588b3f7ccb17cf043a599dcc4bf5de48b7e
Gonzalo Nelis S <gonza.nelis@gmail.com>
2020-10-24 16:59:47 -0300

    Cache Matrix function completed. It is able to store and return its inverse from a previous calculation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a matrix which is capable of caching its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(X = matrix()) {
+    inv <- NULL
+    set <- function(Y){
+        X<<-Y
+        inv<<-NULL
+    }
+    get <-function() X
+    setinv <- function(new_inv) inv<<-new_inv
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse matrix. If it was calculated beforehand, 
+##it omits the solving process an returns the value already calculated
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(X, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    inv <- X$getinv()
+    if(!is.null(inv)){ ##If already calculated, returns the inverse
+        message("getting cached matrix")
+        return(inv)
+    }
+    data<-X$get() ##gets the data if the inverse was not calculated before
+    inv<- solve(data) ##Calculates the inverse
+    X$setinv(inv)  ##set the inverse matrix to the CacheMatrix object
+    inv ##returns the inverse
 }
